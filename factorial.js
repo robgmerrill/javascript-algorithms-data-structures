ada161b7d5f9b1269d9d23b7f29c331a68cd0ae7
robgmerrill <robgmerrill>
2021-05-25 12:46:30 -0700

    Factorial iterative and recursive

---
+++ factorial.js
@@ -0,0 +1,23 @@
+// 4! = 4 * 3 * 2 * 1 = 24
+
+// Iterative Example
+// function factorial(num) {
+//     var total = 1;
+//     for (i = 2; i <= 4; i++) {
+//         total = total * i;
+//     }
+//     return total;
+// }
+
+// factorial(4)
+
+// Recursive
+function factorial(num) {
+  if (num === 1) {
+    console.log("all done");
+    return num;
+  }
+  return num * factorial(num - 1);
+}
+
+factorial(4);
